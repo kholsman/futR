c9c453ad2ed8ee3fb3e380048f720021926a0966
K Holsman <kholsman@users.noreply.github.com>
2025-01-16 17:02:24 -0800

    update

--- R/updatePackage.R
+++ R/updatePackage.R
@@ -4,9 +4,9 @@
 #'
 
 # create the package folder
-usethis::create_package( "/Users/KKH/Documents/GitHub_mac/futR")
-usethis::use_test("test-data")
-source("data-raw/preprocess.R")
-
-# update documentation
-devtools::document(roclets = c('rd', 'collate', 'namespace'))
+# usethis::create_package( "/Users/KKH/Documents/GitHub_mac/futR")
+# usethis::use_test("test-data")
+# source("data-raw/preprocess.R")
+#
+# # update documentation
+# devtools::document(roclets = c('rd', 'collate', 'namespace'))
