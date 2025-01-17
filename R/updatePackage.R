d308e82aabdb534e5d85880f6d0add3747868d51
K Holsman <kholsman@users.noreply.github.com>
2025-01-16 16:47:03 -0800

    add test

--- R/updatePackage.R
+++ R/updatePackage.R
@@ -7,3 +7,6 @@
 usethis::create_package( "/Users/KKH/Documents/GitHub_mac/futR")
 usethis::use_test("test-data")
 source("data-raw/preprocess.R")
+
+# update documentation
+devtools::document(roclets = c('rd', 'collate', 'namespace'))
